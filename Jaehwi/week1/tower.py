3bfa16e2129bb306be18e56df31a880d406efe2f
Jaehwi Song <wotns0319@naver.com>
2023-12-18 05:20:46 +0000

    2493 tower

---
+++ Jaehwi/week1/tower.py
@@ -0,0 +1,58 @@
+# tower_num = int(input())
+# tower_height = list(map(int, input().split()))
+
+# for idx in range(0, len(tower_height)):
+#     if idx == 0:
+#         print(0, end=" ")
+#         continue
+    
+#     for comp in range(idx-1, -1, -1):
+#         if tower_height[idx] <= tower_height[comp]:
+#             print(comp+1, end=" ")
+#             break
+        
+#         if comp == 0:
+#             print(0, end=" ")
+
+
+
+
+# for idx in range(len(tower_height)-1, 0, -1):
+#     flag = True
+#     for comp in range(idx-1, -1, -1):
+#         if tower_height[comp] >= tower_height[idx]:
+#             tower_height[idx] = comp+1
+#             flag = False
+#             break
+#     if flag == True:
+#         tower_height[idx] = 0
+
+# tower_height[0] = 0
+
+# for val in tower_height:
+#     print(val, end=" ")
+
+import sys
+
+N = int(sys.stdin.readline())
+towers = list(map(int, sys.stdin.readline().split()))
+
+stack = []
+answer = []
+
+for i in range(N):
+    while stack:
+        if stack[-1][1] > towers[i]:
+            answer.append(stack[-1][0]+1)
+            break
+        else:
+            stack.pop()
+    if not stack:
+        answer.append(0)
+    
+    stack.append([i, towers[i]])
+
+for i in answer:
+    print(i, end=" ")
+
+
